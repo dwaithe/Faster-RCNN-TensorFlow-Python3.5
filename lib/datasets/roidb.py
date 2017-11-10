e14895e54420781db0f5259270a6df99810bbe00
Dominic Waithe <dominic_waithe@hotmail.com>
2017-11-10 14:19:29 +0000

    debug cont

--- lib/datasets/roidb.py
+++ lib/datasets/roidb.py
@@ -26,7 +26,7 @@ def prepare_roidb(imdb):
          for i in range(imdb.num_images)]
   for i in range(len(imdb.image_index)):
     roidb[i]['image'] = imdb.image_path_at(i)
-    
+    print ('test',roidb[i])
     if not (imdb.name.startswith('coco')):
       roidb[i]['width'] = sizes[i][0]
       roidb[i]['height'] = sizes[i][1]
