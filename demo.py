390f047923190674315b5d8fe1c9d5f615a03fa1
Dominic Waithe <dominic_waithe@hotmail.com>
2018-02-22 16:27:05 +0000

    demo update

--- demo.py
+++ demo.py
@@ -159,7 +159,7 @@ def demo(sess, net, image_name):
 
     # Visualize detections for each class
     CONF_THRESH = 0.1
-    NMS_THRESH = 0.7
+    NMS_THRESH = 1.0
     for cls_ind, cls in enumerate(cfg.FLAGS2["CLASSES"][1:]):
         cls_ind += 1  # because we skipped background
         cls_boxes = boxes[:, 4 * cls_ind:4 * (cls_ind + 1)]
